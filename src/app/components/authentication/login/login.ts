8f3807937263e2fbb85281da2d4dd6815370adb0
SK ARBAJ ALI <skarbajali.one@outlook.com>
2025-12-27 19:19:55 +0530

    fix : added the reload and navbar problem

--- src/app/components/authentication/login/login.ts
+++ src/app/components/authentication/login/login.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, input } from '@angular/core';
+import { Component, effect, inject, input, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -17,26 +17,32 @@ export class Login {
     email: new FormControl(''),
     password: new FormControl('')
   });
+  isLoggedIn = signal(false);
 
-  constructor(){
-    const userData = JSON.parse(localStorage.getItem('userData')!);
-    if(userData){
-      if(userData?.user?.role.toLowerCase() === 'admin'){
-        this.router.navigateByUrl('/question-panel');
-      }
-      else{
-        this.router.navigateByUrl('');
+  constructor() {
+    effect(() => {
+      if (this.isLoggedIn()) {
+        const userData = JSON.parse(localStorage.getItem('userData')!);
+        if (userData) {
+          location.href = 'https://question-form-frontend.onrender.com/'
+          // location.href = 'http://localhost:4200'
+        }
+        else {
+          this.router.navigateByUrl('');
+        }
       }
     }
-  }
+  )
 
-  onSubmit(){
-    this.http.post('https://question-form-backend.onrender.com/api/v1/users/login',{...(this.loginForm.value)})
-    .subscribe((res:any)=>{
-      localStorage.setItem('userData',JSON.stringify(res?.data));
+}
+
+onSubmit() {
+  this.http.post('https://question-form-backend.onrender.com/api/v1/users/login', { ...(this.loginForm.value) })
+    .subscribe((res: any) => {
+      localStorage.setItem('userData', JSON.stringify(res?.data));
       alert(`Login Successful : ${res?.data?.user?.name}`);
-      location.reload();
-      
+      // location.reload();
+      this.isLoggedIn.set(true);
     });
-  }
+}
 }
